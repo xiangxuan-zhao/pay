09f5a5b57e68523c3d9858d2a83290c3b141f7dc
doremi_xiangxuan <https://git.code.tencent.com/ceteng-duolaimi/ExamServer.git>
2020-04-01 18:47:19 +0800

    搭建项目环境

---
+++ src/test/java/com/imooc/pay/PayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.imooc.pay;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
