547d21230c9e7659468adcbb76b7793095d1058e
doremi_xiangxuan <https://git.code.tencent.com/ceteng-duolaimi/ExamServer.git>
2020-04-01 21:42:00 +0800

    完成微信native发起支付

--- src/main/java/com/imooc/controller/PayController.java
+++ src/main/java/com/imooc/controller/PayController.java
@@ -5,10 +5,7 @@ import com.lly835.bestpay.model.PayResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
